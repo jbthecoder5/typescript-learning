339f46552696781409e03e2011fd6951d621f480
Joseph Borne <josephpbourne@outlook.com>
2022-12-08 15:30:22 -0800

    some more practicing with typescript template strings

--- ch01/hello_typescript.js
+++ ch01/hello_typescript.js
@@ -1,2 +1,4 @@
-var version = "es6";
-console.log("hello ".concat(version, " Typescript"));
+var fname = "joseph"; // my first name
+var mname = "pierre"; // my middle name
+var lname = "borne"; // my last name
+console.log("My first name is: ".concat(fname, "\n, \nmy middle name is: ").concat(mname, "\n,\nmy last name is: ").concat(lname, " "));
--- ch01/hello_typescript.ts
+++ ch01/hello_typescript.ts
@@ -1,2 +1,7 @@
-var version = `es6`;
-console.log(`hello ${version} Typescript`);
+var fname = `joseph`; // my first name
+var mname = `pierre`; // my middle name
+var lname = `borne`; // my last name
+
+console.log(`My first name is: ${fname}\n, 
+my middle name is: ${mname}\n,
+my last name is: ${lname} `);
