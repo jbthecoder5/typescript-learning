e4d7d8d619c949f33122fe30327ca96df4f56a69
Joseph Borne <josephpbourne@outlook.com>
2022-12-21 12:29:46 -0800

    did a little more practice with different strong typed data types in ts

--- ch01/hello_typescript.ts
+++ ch01/hello_typescript.ts
@@ -2,3 +2,12 @@ var myBoolean: boolean = true;
 var myNum: number = 23;
 var myString: string = "james";
 var myArry: string[] = ["zero", "first", "second", "third"];
+
+myBoolean = myNum === 456;
+console.log(`my boolean = ${myBoolean}`);
+
+myArry = [myNum.toString(), `5678`];
+console.log(`myArray = ${myArry}`);
+
+myNum = myArry.length;
+console.log(`myNum = ${myNum}`);
